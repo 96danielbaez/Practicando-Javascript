298b403c0c4807ecef2a916f7624238ef4d5f52f
96danielbaez <danworknet@gmail.com>
2023-06-25 12:01:25 -0300

    25-6-23 - Arrays

---
+++ arrays.js
@@ -0,0 +1,22 @@
+// Creamos y cargamos vectores, vacios o con una capacidad determinada.
+
+var vector = []; // para cargar después
+var vector2= [4]; // vector de 5 posiciones, recordar que vel indice arranca en 0.
+
+console.log(vector.length);
+vector[1] = "Osher"; // Cargamos el indice 1 (sería la segunda posición).
+
+console.log(vector[1]);
+console.log(vector.length);
+console.log(vector);
+
+
+// Recordar qué con el parametro .length sabremos la capacidad de nuestro vector.
+
+var vector2 = ["Pedro", , "Juan", "Asd"];
+console.log(vector2);
+console.log(vector2.length);
+
+
+
+
